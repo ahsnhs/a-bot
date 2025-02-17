42813b131892316ce02c370846a4e02c74351842
ahsnhs <158833847+ahsnhs@users.noreply.github.com>
2025-02-17 03:31:38 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("prev").addEventListener("click", function() {
+    alert("تم الضغط على زر السابق!");
+});
+
+document.getElementById("next").addEventListener("click", function() {
+    alert("تم الضغط على زر التالي!");
+});
